7e845360e0d221308fabf076d045238a154c5c68
s24729-pj <s24729@pjwstk.edu.pl>
2025-05-16 18:22:01 +0200

    Works on chats component

---
+++ src/components/Chats.jsx
@@ -0,0 +1,12 @@
+function Chats() {
+  return (
+    <div id="chats">
+      <h2>Chats</h2>
+      <div class="test-chat"></div>
+      <div class="test-chat"></div>
+      <div class="test-chat"></div>
+    </div>
+  );
+}
+
+export default Chats;
